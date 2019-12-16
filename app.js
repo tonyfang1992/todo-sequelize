ce8615f9f1ab9938bac668bef8da82562c537267
tonyfang <q710370@gmail.com>
2019-12-16 19:18:30 +0800

    Add mysql database

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const methodOverride = require('method-override')
 
 const port = 3000
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -25,7 +29,7 @@ app.get('/users/login', (req, res) => {
 })
 //登入檢查
 app.post('/users/login', (req, res) => {
-  res.send('login')
+
 })
 //註冊頁面
 app.get('/users/register', (req, res) => {
@@ -33,7 +37,11 @@ app.get('/users/register', (req, res) => {
 })
 //註冊檢查
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => res.redirect('/'))
 })
 //登出
 app.get('/users/logout', (req, res) => {
