cdcde43d00fb77941fcc1da0b2c95793613de9b9
tonyfang <q710370@gmail.com>
2019-12-18 15:21:17 +0800

    Add facebook login functionity and remove fb secret key

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
+  require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
+}
 
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -42,6 +45,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
 app.use('/todos', require('./routes/todo'))
+app.use('/auth', require('./routes/auths'))
 
 
 
